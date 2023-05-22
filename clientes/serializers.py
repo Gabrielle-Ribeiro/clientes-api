35c6f5b65c0b5aaf167d5f549dfaa8239a04fed9
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2023-05-22 19:38:35 -0300

    Adiciona mais validações para cpf e celular

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -9,11 +9,11 @@ class ClienteSerializer(serializers.ModelSerializer):
     
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({"cpf":"O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({"cpf":"Número de CPF inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({"nome":"Não inclua números nesse campo"})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({"rg":"O RG deve ter 9 dígitos"})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({"celular":"O celular deve ter no mínimo 11 dígitos"})
+            raise serializers.ValidationError({"celular":"O número de celular deve seguir este modelo: XX XXXXX-XXXX"})
         return data
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,5 +1,9 @@
-def cpf_valido(cpf):
-    return len(cpf) == 11
+import re
+from validate_docbr import CPF
+
+def cpf_valido(numero_cpf):
+    cpf = CPF()
+    return cpf.validate(numero_cpf)
         
 def nome_valido(nome):
     return nome.isalpha()
@@ -8,5 +12,6 @@ def rg_valido(rg):
     return len(rg) == 9
             
 def celular_valido(celular):
-    return len(celular) >= 11
-       
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta
