782890074b2b75bab81cc51cf7459e6a51fb77fb
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2023-05-22 20:14:28 -0300

    Adiciona busca e filtro

--- clientes/views.py
+++ clientes/views.py
@@ -7,7 +7,9 @@ class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ["nome"]
+    search_fields = ['nome', 'cpf']
+    filterset_fields = ['ativo']
 
 
